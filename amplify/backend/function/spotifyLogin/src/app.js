aa49e0bb888fc226ab191cd4419e38eaf1879a6d
Shelby Poliachik <spoliachik@captechconsulting.com>
2021-10-25 15:48:33 -0400

    added api & changed useAuth to call it

--- amplify/backend/function/spotifyLogin/src/app.js
+++ amplify/backend/function/spotifyLogin/src/app.js
@@ -96,8 +96,21 @@ var express = require('express');
 const cors = require('cors');
 const spotifyWebApi = require('spotify-web-api-node');
 
-var app = express();
-const port = 8000
+var express = require('express')
+var bodyParser = require('body-parser')
+var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+// declare a new express app
+var app = express()
+app.use(bodyParser.json())
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+// Enable CORS for all methods
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+});
 
 app.use(cors()) // to handle cross-origin requests
 app.use(express.json()); // to parse JSON bodies
@@ -108,7 +121,7 @@ var clientSecret = process.env.CLIENT_SECRET;
 const credentials = {
     clientId: clientId,
     clientSecret: clientSecret,
-    redirectUri: 'http://localhost:3000/'
+    redirectUri: 'https://master.d139yngib8df9c.amplifyapp.com/'
 }
 
 app.get('/', (req, res) => {
--- src/useAuth.js
+++ src/useAuth.js
@@ -6,21 +6,30 @@ export default function useAuth(givenCode) {
     const code = (givenCode.code.code)
 
     useEffect(() => {
-        axios
-            .post("http://localhost:8000/login", { code })
-            .then((response) => {
-                console.log("successfully in useAuth .then")
-                //if success then cut the code string from the URL and execute next thing
-                // window.history.pushState({}, null, "/");
-                console.log(response.data);
-                setAccessToken(response.data.accessToken);
-        })
-        .catch((err) => {
-            //if fail, redirect to home page (Login page)
-            console.log("catch")
+        // axios
+        //     .post("http://localhost:8000/login", { code })
+        //     .then((response) => {
+        //         console.log("successfully in useAuth .then")
+        //         //if success then cut the code string from the URL and execute next thing
+        //         // window.history.pushState({}, null, "/");
+        //         console.log(response.data);
+        //         setAccessToken(response.data.accessToken);
+        // })
+        // .catch((err) => {
+        //     //if fail, redirect to home page (Login page)
+        //     console.log("catch")
+        //     console.log(err);
+        //     window.location = "/";
+        // });
+        try {
+            const response = API.post('spotifyAPI', '/spotifyLogin', { code });
+            console.log(response.data);
+            setAccessToken(response.data.accessToken);
+        } catch (err) {
+            console.log('catch');
             console.log(err);
             window.location = "/";
-        });
+        }
     }, [code]);
 
     return accessToken;
