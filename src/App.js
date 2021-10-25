47164242b8cd26a8e1c4d1450f501c931e20361a
Shelby Poliachik <spoliachik@captechconsulting.com>
2021-10-25 11:40:04 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from "./Login";
+import SongSearch from "./SongSearch";
+import useAuth from "./useAuth";
+// import makeSpotifyApi from './spotify';
+
+
+// TO START SERVER RUN node server.js in separate terminal, then npm run start to start react app
+
+const code = new URLSearchParams(window.location.search).get('code');
 
 function App() {
+  console.log(code);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {code ? <SongSearch code={code}/> : <Login />}
     </div>
   );
 }
